ff65577152ef3036ad4e93dc41f0339234de6baf
biobios <teiketsuatsu.lbp@gmail.com>
2023-07-12 23:48:21 +0900

    for constructing global object

---
+++ src/spec/icxxabi.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#define ATEXIT_MAX_FUNCS 128
+
+extern "C" {
+
+typedef unsigned uarch_t;
+
+struct atexit_func_entry_t {
+    void (*destructor_func)(void*);
+    void* obj_ptr;
+    void* dso_handle;
+};
+
+int __cxa_atexit(void (*f)(void*), void* objptr, void* dso);
+void __cxa_finalize(void* f);
+}
