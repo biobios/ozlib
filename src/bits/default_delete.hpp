64eb780ec7e1a97524d174fd3d288552ea58bdd6
BioBIOS <biobios1024@gmail.com>
2024-08-05 03:14:54 +0900

    feat: Impl default_delete

---
+++ src/bits/default_delete.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <bits/is_convertible.hpp>
+#include <bits/is_void.hpp>
+
+namespace std {
+namespace impl {
+template <typename T>
+struct default_delete {
+    constexpr default_delete() noexcept = default;
+    template <typename From>
+        requires is_convertible_v<From*, T*>
+    default_delete(const default_delete<From>&) noexcept {}
+    void operator()(T* ptr) const {
+        static_assert(!is_void_v<T>, "can't delete pointer to incomplete type");
+        static_assert(sizeof(T) > 0, "can't delete pointer to incomplete type");
+        delete ptr;
+    }
+};
+template <typename T>
+struct default_delete<T[]> {
+    constexpr default_delete() noexcept = default;
+
+    template <typename From>
+        requires is_convertible_v<From (*)[], T (*)[]>
+    default_delete(const default_delete<From[]>&) noexcept {}
+
+    template <typename From>
+        requires is_convertible_v<From (*)[], T (*)[]>
+    void operator()(From* ptr) const {
+        static_assert(sizeof(T) > 0, "can't delete pointer to incomplete type");
+        delete[] ptr;
+    }
+};
+}  // namespace impl
+}  // namespace std
