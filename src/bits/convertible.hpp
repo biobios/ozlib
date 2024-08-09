6f986f1fd5684d4fc43ab5beb11fbe9b44152d7a
BioBIOS <biobios1024@gmail.com>
2024-08-10 08:43:33 +0900

    refactor: Rename is_convertible.hpp to convertible.hpp and update references

--- src/bits/convertible.hpp
+++ src/bits/convertible.hpp
@@ -34,6 +34,13 @@ namespace helper_is_convertible {};
 
 template <typename From, typename To>
 inline constexpr bool is_convertible_v = is_convertible<From, To>::value;
+
+template <class From, class To>
+concept convertible_to = impl::is_convertible_v<From, To> &&
+                         requires(impl::add_rvalue_reference_t<From> (&f)()) {
+                             static_cast<To>(f());
+                         };
+
 }  // namespace impl
 
 }  // namespace std
