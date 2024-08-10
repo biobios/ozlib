ba5427d0699712701250055bcced8911a1b1d94d
BioBIOS <biobios1024@gmail.com>
2024-08-11 02:56:29 +0900

    feat: Add is_bounded_array and is_unbounded_array

--- src/bits/array_traits.hpp
+++ src/bits/array_traits.hpp
@@ -7,10 +7,25 @@ namespace std {
 namespace impl {
 
 template <typename>
-struct is_array : false_type {};
+struct is_bounded_array : false_type {};
 
 template <typename T, size_t size>
-struct is_array<T[size]> : true_type {};
+struct is_bounded_array<T[size]> : true_type {};
+
+template <typename T>
+inline constexpr bool is_bounded_array_v = is_bounded_array<T>::value;
+
+template <typename>
+struct is_unbounded_array : false_type {};
+
+template <typename T>
+struct is_unbounded_array<T[]> : true_type {};
+
+template <typename T>
+inline constexpr bool is_unbounded_array_v = is_unbounded_array<T>::value;
+
+template <typename>
+struct is_array : false_type {};
 
 template <typename T>
 struct is_array<T[]> : true_type {};
