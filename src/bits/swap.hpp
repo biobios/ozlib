5f43dac8f23e2ade1c7da2741b893de603f59e69
BioBIOS <biobios1024@gmail.com>
2024-08-15 02:19:33 +0900

    feat: Add swap function for generic types and arrays

---
+++ src/bits/swap.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <bits/assignable.hpp>
+#include <bits/constructible.hpp>
+#include <bits/size_t.hpp>
+
+namespace std {
+namespace impl {
+
+template <typename T>
+constexpr void swap(T& a, T& b) noexcept(is_nothrow_move_constructible_v<T> &&
+                                         is_nothrow_move_assignable_v<T>) {
+    T tmp(std::move(a));
+    a = std::move(b);
+    b = std::move(tmp);
+}
+
+template <typename T, size_t N>
+constexpr void swap(T (&a)[N], T (&b)[N]) noexcept(noexcept(swap(*a, *b))) {
+    for (size_t i = 0; i < N; ++i) {
+        swap(a[i], b[i]);
+    }
+}
+
+}  // namespace impl
+}  // namespace std
