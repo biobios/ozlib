b5c9aa27042be6312e9a1a57848211d03246fa43
BioBIOS <biobios1024@gmail.com>
2024-08-15 02:19:15 +0900

    feat: Add is_constructible_v, is_default_constructible_v, is_copy_constructible_v, is_move_constructible_v, is_nothrow_constructible_v, is_nothrow_default_constructible_v, is_nothrow_copy_constructible_v, is_nothrow_move_constructible_v type traits

--- src/bits/constructible.hpp
+++ src/bits/constructible.hpp
@@ -17,18 +17,68 @@ template <typename T, typename... Args>
 struct is_constructible<T, Args...> : true_type {};
 
 template <typename T>
+inline constexpr bool is_constructible_v = is_constructible<T>::value;
+
+template <typename T>
 struct is_default_constructible : is_constructible<T>::type {};
 
 template <typename T, size_t N>
 struct is_default_constructible<T[N]> : is_constructible<T>::type {};
 
+template <typename T>
+inline constexpr bool is_default_constructible_v =
+    is_default_constructible<T>::value;
+
 // C++11準拠　C++14へ修正が必要
 template <typename T>
 struct is_copy_constructible : is_constructible<T, const T&> {};
 
+template <typename T>
+inline constexpr bool is_copy_constructible_v = is_copy_constructible<T>::value;
+
 // C++11準拠　C++14へ修正が必要
 template <typename T>
 struct is_move_constructible : is_constructible<T, T&&> {};
+
+template <typename T>
+inline constexpr bool is_move_constructible_v = is_move_constructible<T>::value;
+
+template <typename T, typename... Args>
+struct is_nothrow_constructible : false_type {};
+
+template <typename T, typename... Args>
+    requires(requires(Args... args) {
+        { T(args...) } noexcept;
+    } && !is_void_v<T>)
+struct is_nothrow_constructible<T, Args...> : true_type {};
+
+template <typename T>
+inline constexpr bool is_nothrow_constructible_v =
+    is_nothrow_constructible<T>::value;
+
+template <typename T>
+struct is_nothrow_default_constructible : is_nothrow_constructible<T> {};
+
+template <typename T, size_t N>
+struct is_nothrow_default_constructible<T[N]> : is_nothrow_constructible<T> {};
+
+template <typename T>
+inline constexpr bool is_nothrow_default_constructible_v =
+    is_nothrow_default_constructible<T>::value;
+
+template <typename T>
+struct is_nothrow_copy_constructible : is_nothrow_constructible<T, const T&> {};
+
+template <typename T>
+inline constexpr bool is_nothrow_copy_constructible_v =
+    is_nothrow_copy_constructible<T>::value;
+
+template <typename T>
+struct is_nothrow_move_constructible : is_nothrow_constructible<T, T&&> {};
+
+template <typename T>
+inline constexpr bool is_nothrow_move_constructible_v =
+    is_nothrow_move_constructible<T>::value;
 
 }  // namespace impl
 
