57554408988186a54e6289548ed89528a22b2510
BioBIOS <biobios1024@gmail.com>
2024-08-15 03:13:05 +0900

    feat: Implement unique_ptr for array

--- src/bits/unique_ptr.hpp
+++ src/bits/unique_ptr.hpp
@@ -149,6 +149,152 @@ class unique_ptr {
     constexpr pointer operator->() const noexcept { return get(); }
 };
 
+/// @brief
+/// リソースへのポインタの所有権を唯一持つスマートポインタ、配列版
+/// @tparam T リソースの型
+/// @tparam D リソースの解放方法
+template <typename T, typename D>
+class unique_ptr<T[], D> {
+    template <typename Elem, typename Del>
+    struct get_pointer_type {
+        using type = Elem*;
+    };
+
+    template <typename Elem, typename Del>
+        requires requires() { typename Del::pointer; }
+    struct get_pointer_type<Elem, Del> {
+        using type = typename Del::pointer;
+    };
+
+   public:
+    using pointer = typename get_pointer_type<T, D>::type;
+    using deleter_type = D;
+    using element_type = T;
+
+    constexpr unique_ptr() noexcept
+        requires(is_nothrow_default_constructible_v<deleter_type> &&
+                 !is_pointer_v<deleter_type>)
+        : _ptr(nullptr), _deleter() {}
+    {}
+
+    template <typename U>
+        requires(is_nothrow_default_constructible_v<deleter_type> &&
+                 !is_pointer_v<deleter_type> &&
+                 (is_same_v<U, pointer> ||
+                  (is_same_v<pointer, element_type*> && is_pointer_v<U> &&
+                   is_convertible_v<remove_pointer_t<U> (*)[],
+                                    element_type (*)[]>)))
+    constexpr explicit unique_ptr(U p) noexcept : _ptr(p), _deleter() {}
+
+    template <typename U>
+        requires(is_nothrow_copy_constructible_v<deleter_type> &&
+                 (is_same_v<U, pointer> ||
+                  (is_same_v<pointer, element_type*> && is_pointer_v<U> &&
+                   is_convertible_v<remove_pointer_t<U> (*)[],
+                                    element_type (*)[]>) ||
+                  is_same_v<U, nullptr_t>))
+    constexpr unique_ptr(U p, const D& d1) noexcept : _ptr(p), _deleter(d1) {}
+
+    template <typename U>
+        requires(is_nothrow_move_constructible_v<deleter_type> &&
+                 (is_same_v<U, pointer> ||
+                  (is_same_v<pointer, element_type*> && is_pointer_v<U> &&
+                   is_convertible_v<remove_pointer_t<U> (*)[],
+                                    element_type (*)[]>) ||
+                  is_same_v<U, nullptr_t>))
+    constexpr unique_ptr(U p, D&& d2) noexcept
+        : _ptr(p), _deleter(forward<D>(d2)) {}
+
+    constexpr unique_ptr(unique_ptr&& u) noexcept
+        requires is_nothrow_move_constructible_v<deleter_type>
+        : _ptr(u.release()), _deleter(forward<D>(u.get_deleter())) {}
+
+    constexpr unique_ptr(nullptr_t) noexcept
+        requires is_nothrow_default_constructible_v<deleter_type>
+        : _ptr(nullptr), _deleter() {}
+
+    template <typename U, typename E>
+    constexpr unique_ptr(unique_ptr<U, E>&& u) noexcept
+        requires(!is_array_v<U> && is_same_v<pointer, element_type*> &&
+                 is_same_v<typename unique_ptr<U, E>::pointer,
+                           typename unique_ptr<U, E>::element_type*> &&
+                 is_convertible_v<typename unique_ptr<U, D>::element_type (*)[],
+                                  element_type (*)[]> &&
+                 ((is_same_v<D, E> && is_reference_v<D>) ||
+                  (!is_reference_v<D> && is_convertible_v<E, D>)))
+        : _ptr(u.release()), _deleter(forward<E>(u.get_deleter())) {}
+
+    unique_ptr(const unique_ptr&) = delete;
+
+    constexpr ~unique_ptr() {
+        if (get() != nullptr) {
+            get_deleter()(get());
+        }
+    }
+
+    constexpr unique_ptr& operator=(unique_ptr&& u) noexcept {
+        reset(u.release());
+        _deleter = forward<D>(u.get_deleter());
+    }
+
+    template <typename U, typename E>
+        requires(is_array_v<U> && is_same_v<pointer, element_type*> &&
+                 is_same_v<typename unique_ptr<U, E>::pointer,
+                           typename unique_ptr<U, E>::element_type*> &&
+                 is_convertible_v<typename unique_ptr<U, D>::element_type (*)[],
+                                  element_type (*)[]> &&
+                 is_assignable_v<D&, E &&>)
+    constexpr unique_ptr& operator=(unique_ptr<U, E>&& u) noexcept {
+        reset(u.release());
+        _deleter = forward<E>(u.get_deleter());
+    }
+
+    constexpr unique_ptr& operator=(nullptr_t) noexcept { reset(); }
+
+    unique_ptr& operator=(const unique_ptr&) = delete;
+
+    constexpr pointer release() noexcept {
+        pointer p = get();
+        _ptr = nullptr;
+        return p;
+    }
+
+    constexpr void reset(pointer p = pointer()) noexcept {
+        pointer old = get();
+        _ptr = p;
+        if (old != nullptr) {
+            get_deleter()(old);
+        }
+    }
+
+    constexpr void reset(nullptr_t = nullptr) noexcept { reset(); }
+
+    template <typename U>
+        requires(
+            is_same_v<U, pointer> ||
+            (is_same_v<pointer, element_type*> && is_pointer_v<U> &&
+             is_convertible_v<remove_pointer_t<U> (*)[], element_type (*)[]>))
+    constexpr void reset(U p) noexcept {
+        reset(static_cast<pointer>(p));
+    }
+
+    constexpr void swap(unique_ptr& u) noexcept {
+        swap(_ptr, u._ptr);
+        swap(_deleter, u._deleter);
+    }
+
+    constexpr pointer get() const noexcept { return _ptr; }
+
+    constexpr const deleter_type& get_deleter() const noexcept {
+        return _deleter;
+    }
+
+    constexpr explicit operator bool() const noexcept {
+        return get() != nullptr;
+    }
+
+    constexpr T& operator[](size_t i) const { return get()[i]; }
+};
 
 template <typename T1, typename D1, typename T2, typename D2>
 constexpr bool operator==(const unique_ptr<T1, D1>& x,
