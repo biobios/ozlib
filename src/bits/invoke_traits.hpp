41fb638cf35cf812368c5e47ab962147a5962979
BioBIOS <biobios1024@gmail.com>
2024-11-06 20:12:32 +0900

    fix: Fix a bug that invoke_result don't work properly when F contains cv-qualification or is a reference type.

--- src/bits/invoke_traits.hpp
+++ src/bits/invoke_traits.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <bits/cvref_traits.hpp>
+#include <bits/decay.hpp>
+#include <bits/declval.hpp>
 #include <bits/fundamental_traits.hpp>
 #include <bits/inheritance_traits.hpp>
 #include <bits/template_logics.hpp>
@@ -11,66 +13,82 @@ namespace impl {
 template <typename T>
 class reference_wrapper;
 
+namespace helper_invoke_result {
+
 template <typename...>
-struct invoke_result {};
+struct invoke_result_helper {};
 
-template <typename R, typename T1, typename... Args1, typename T2,
-          typename... Args2>
-    requires(is_base_of_v<T1, remove_cvref_t<T2>> &&
-             requires(R (T1::*f)(Args1...), T2 t2, Args2... args2) {
-                 (t2.*f)(args2...);
-             })
-struct invoke_result<R (T1::*)(Args1...), T2, Args2...> {
-    using type = R;
+template <typename F, typename T1, typename T2, typename... Args>
+    requires(is_base_of_v<T1, remove_cvref_t<T2>> && is_function_v<F> &&
+             requires(F T1::*f, T2 t2, Args... args) { (t2.*f)(args...); })
+struct invoke_result_helper<F T1::*, T2, Args...> {
+    using type =
+        decltype((declval<T2>().*declval<F T1::*>())(declval<Args>()...));
 };
 
-template <typename R, typename T1, typename... Args1, typename T2,
-          typename... Args2>
-    requires(is_same_v<reference_wrapper<T1>, remove_cvref_t<T2>> &&
-             requires(R (T1::*f)(Args1...), T2 t2, Args2... args2) {
-                 (t2.get().*f)(args2...);
-             })
-struct invoke_result<R (T1::*)(Args1...), T2, Args2...> {
-    using type = R;
+template <typename F, typename T1, typename T2, typename... Args>
+    requires(
+        is_same_v<reference_wrapper<T1>, remove_cvref_t<T2>> &&
+        is_function_v<F> &&
+        requires(F T1::*f, T2 t2, Args... args) { (t2.get().*f)(args...); })
+struct invoke_result_helper<F T1::*, T2, Args...> {
+    using type =
+        decltype((declval<T2>().get().*declval<F T1::*>())(declval<Args>()...));
 };
 
-template <typename R, typename T1, typename... Args1, typename T2,
-          typename... Args2>
-    requires(requires(R (T1::*f)(Args1...), T2 t2, Args2... args2) {
-        ((*t2).*f)(args2...);
-    })
-struct invoke_result<R (T1::*)(Args1...), T2, Args2...> {
-    using type = R;
+template <typename F, typename T1, typename T2, typename... Args>
+    requires(requires(F T1::*f, T2 t2, Args... args) { ((*t2).*f)(args...); })
+struct invoke_result_helper<F T1::*, T2, Args...> {
+    using type =
+        decltype(((*declval<T2>()).*declval<F T1::*>())(declval<Args>()...));
 };
 
 template <typename R, typename T, typename Arg>
-    requires(is_base_of_v<T, remove_cvref_t<Arg>> &&
+    requires(!is_function_v<R> && is_base_of_v<T, remove_cvref_t<Arg>> &&
              requires(R T::*f, Arg t1) { (t1.*f); })
-struct invoke_result<R T::*, Arg> {
+struct invoke_result_helper<R T::*, Arg> {
     using type = R;
 };
 
 template <typename R, typename T, typename Arg>
-    requires(is_same_v<reference_wrapper<T>, remove_cvref_t<Arg>> &&
+    requires(!is_function_v<R> &&
+             is_same_v<reference_wrapper<T>, remove_cvref_t<Arg>> &&
              requires(R T::*f, Arg t1) { (t1.get().*f); })
-struct invoke_result<R T::*, Arg> {
+struct invoke_result_helper<R T::*, Arg> {
     using type = R;
 };
 
 template <typename R, typename T, typename Arg>
-    requires(requires(R T::*f, Arg t1) { ((*t1).*f); })
-struct invoke_result<R T::*, Arg> {
+    requires(!is_function_v<R> && requires(R T::*f, Arg t1) { ((*t1).*f); })
+struct invoke_result_helper<R T::*, Arg> {
     using type = R;
 };
 
 template <typename F, typename... Args>
     requires(requires(F f, Args... args) { f(args...); })
-struct invoke_result<F, Args...> {
+struct invoke_result_helper<F, Args...> {
     using type = decltype(declval<F>()(declval<Args>()...));
 };
 
-template <typename T, typename... Args>
-using invoke_result_t = typename invoke_result<T, Args...>::type;
+}  // namespace helper_invoke_result
+
+template <typename F, typename... Args>
+struct invoke_result : helper_invoke_result::invoke_result_helper<
+                           conditional_t<is_class_v<remove_cvref_t<F>> ||
+                                             is_union_v<remove_cvref_t<F>>,
+                                         F, remove_cvref_t<F>>,
+                           Args...> {
+    // remove_cvref_t<F>が
+    // メンバ変数ポインタ、メンバ関数ポインタ、関数ポインタであれば、remove_cvref_t<F>
+    // オブジェクト(class or union)の時は、
+    // Fをそのままinvoker_result_helperに渡す
+
+    // オブジェクトの場合は、それのconst、volatile、右辺値、左辺値どれであるかによって
+    // 呼び出される関数が変わることがあるから
+};
+
+template <typename F, typename... Args>
+using invoke_result_t = typename invoke_result<F, Args...>::type;
 
 }  // namespace impl
 }  // namespace std
