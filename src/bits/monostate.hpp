2f88f4f641d92ce2916b5ebf8a04fb030612748c
BioBIOS <biobios1024@gmail.com>
2026-08-07 23:33:25 +0900

    wip

--- src/bits/monostate.hpp
+++ src/bits/monostate.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <compare>
+
 namespace std {
 namespace impl {
 struct monostate {};
+
+constexpr std::strong_ordering operator<=>(const monostate&, const monostate&) noexcept {
+    return std::strong_ordering::equal;
+}
 }
 }
